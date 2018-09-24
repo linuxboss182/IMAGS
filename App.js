5f4cb3bf63c5ba73958f10e866d7c8fb5b3c574e
Jesse Gaulin <jtgaulin@wpi.edu>
2018-09-24 15:15:57 -0400

    Setup StackNavigator for screen switching, refactored code

--- App.js
+++ App.js
@@ -8,42 +8,15 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+var { InitialScreen } = require("./InitialScreen.js");
+var { MainScreen } = require("./MainScreen.js");
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>HELLO WORLD!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+export default App = StackNavigator({
+    initial: { screen:InitialScreen },
+    main: { screen:MainScreen },
+},{
+    headerMode: 'screen',
 });
