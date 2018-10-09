ff178e5bdceb51893e6b6993c35e5b7f39a23a84
Jesse Gaulin <jtgaulin@wpi.edu>
2018-10-09 15:24:31 -0400

    Add emojis for pain

--- comps/slider.js
+++ comps/slider.js
@@ -17,35 +17,55 @@ const PainSlider = ({
     pain,
     onValueChange
 }) => (
-    <View style={styles.sliderbox}>
-        <Slider style={styles.slider}
-                value={pain}
-                minimumValue={0}
-                maximumValue={10}
-                onValueChange={onValueChange}
-                minimumTrackTintColor='#226FB2'
-                maximumTrackTintColor='#226FB2'
-                trackStyle={styles.track}
-                thumbStyle={styles.thumb}
-        />
+    <View style={styles.container}>
+        <TouchableOpacity>
+            <Image style={styles.button}
+                   source={require('../img/sad-50.png')} />
+        </TouchableOpacity>
+
+        <View style={styles.sliderbox}>
+            <Slider style={styles.slider}
+                    value={pain}
+                    minimumValue={0}
+                    maximumValue={10}
+                    onValueChange={onValueChange}
+                    minimumTrackTintColor='#226FB2'
+                    maximumTrackTintColor='#226FB2'
+                    trackStyle={styles.track}
+                    thumbStyle={styles.thumb}
+            />
+        </View>
+
+        <TouchableOpacity>
+            <Image style={styles.button}
+                   source={require('../img/happy-50.png')} />
+        </TouchableOpacity>
     </View>
 );
 
 export default PainSlider;
 
 const styles = StyleSheet.create({
-    sliderbox: {
+    container: {
         marginTop: 30,
         marginLeft: 20,
         marginRight: 20,
         backgroundColor: 'rgba(50, 50, 50, 0.70)',
         borderWidth: 1,
         borderColor: 'black',
-        borderRadius: 72 / 2
+        borderRadius: 72 / 2,
+        flexDirection: 'row',
+        alignContent: 'center',
+        justifyContent: 'center'
+    },
+    sliderbox: {
+        flex: 1,
+        justifyContent: 'center'
+
     },
     slider: {
-        marginLeft: 20,
-        marginRight: 20,
+        marginLeft: 5,
+        marginRight: 5,
     },
     track: {
         shadowColor: 'white',
@@ -61,6 +81,9 @@ const styles = StyleSheet.create({
         borderColor: 'lightgray',
         borderWidth: 2,
         borderRadius: 10,
+    },
+    button: {
+        opacity: 0.72,
     }
 });
 
